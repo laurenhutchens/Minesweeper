80dac919577cf70f53e5e589c641642476c3f8e9
Arie Gerard <agerard3@my.gcu.edu>
2025-03-08 01:22:12 -0700

    oifhw

--- MinesweeperGUIAPP/obj/Debug/net8.0-windows/MinesweeperGUIAPP.AssemblyInfo.cs
+++ MinesweeperGUIAPP/obj/Debug/net8.0-windows/MinesweeperGUIAPP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinesweeperGUIAPP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8008eb36c364daefd0569c5d4fa3b8a665841b37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9aef988f56249505106c6dbdb2ce6f87d6466c09")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinesweeperGUIAPP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinesweeperGUIAPP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
