fb547da54672769b409cdb8093b25ba105e621f6
Lauren Hutchens <Laurensh30@gmail.com>
2025-03-08 20:31:45 -0700

    push

--- MineSweeperClasses/obj/Debug/net8.0-windows/MineSweeperClasses.AssemblyInfo.cs
+++ MineSweeperClasses/obj/Debug/net8.0-windows/MineSweeperClasses.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MineSweeperClasses")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67f6b9bd8a88061cef5a4b29186d9f0153a6cb7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cebfcfc3b2bb59db69f15a120f2a22a8b3febf19")]
 [assembly: System.Reflection.AssemblyProductAttribute("MineSweeperClasses")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MineSweeperClasses")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MinesweeperGUIAPP/obj/Debug/net8.0-windows/MinesweeperGUIAPP.AssemblyInfo.cs
+++ MinesweeperGUIAPP/obj/Debug/net8.0-windows/MinesweeperGUIAPP.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("MineSweeperConsole")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("MinesweeperGUIAPP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cebfcfc3b2bb59db69f15a120f2a22a8b3febf19")]
-[assembly: System.Reflection.AssemblyProductAttribute("MineSweeperConsole")]
-[assembly: System.Reflection.AssemblyTitleAttribute("MineSweeperConsole")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e3df746d9340847ea0085b514da2b8d74eedfa8")]
+[assembly: System.Reflection.AssemblyProductAttribute("MinesweeperGUIAPP")]
+[assembly: System.Reflection.AssemblyTitleAttribute("MinesweeperGUIAPP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
