23b2ed722bb59f362ba221e40a048e0d3fb80611
Lauren Hutchens <Laurensh30@gmail.com>
2025-03-09 12:17:15 -0700

    new form (new controls)

---
+++ MinesweeperGUIAPP/FrmPlay.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace MinesweeperGUIAPP
+{
+    partial class FrmPlay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            trackBar1 = new TrackBar();
+            trackBar2 = new TrackBar();
+            label1 = new Label();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar2).BeginInit();
+            SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            trackBar1.Location = new Point(344, 143);
+            trackBar1.Name = "trackBar1";
+            trackBar1.Size = new Size(130, 56);
+            trackBar1.TabIndex = 0;
+            // 
+            // trackBar2
+            // 
+            trackBar2.Location = new Point(344, 299);
+            trackBar2.Name = "trackBar2";
+            trackBar2.Size = new Size(130, 56);
+            trackBar2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(387, 108);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 20);
+            label1.TabIndex = 2;
+            label1.Text = "label1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(387, 276);
+            label2.Name = "label2";
+            label2.Size = new Size(50, 20);
+            label2.TabIndex = 3;
+            label2.Text = "label2";
+            // 
+            // FrmPlay
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(trackBar2);
+            Controls.Add(trackBar1);
+            Name = "FrmPlay";
+            Text = "FrmPlay";
+            ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TrackBar trackBar1;
+        private TrackBar trackBar2;
+        private Label label1;
+        private Label label2;
+    }
+}
