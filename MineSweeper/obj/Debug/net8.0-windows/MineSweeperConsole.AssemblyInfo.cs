39d970c345cbc5c6ed7e8f0ead25bdaef3519bee
Arie Gerard <agerard3@my.gcu.edu>
2025-04-24 14:18:04 -0700

    r

--- MineSweeper/obj/Debug/net8.0-windows/MineSweeperConsole.AssemblyInfo.cs
+++ MineSweeper/obj/Debug/net8.0-windows/MineSweeperConsole.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MineSweeperConsole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e5a3426e393877347003eae9538bfb226f08b3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10314cfd4957594f212e31e23ca022074417ee12")]
 [assembly: System.Reflection.AssemblyProductAttribute("MineSweeperConsole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MineSweeperConsole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
