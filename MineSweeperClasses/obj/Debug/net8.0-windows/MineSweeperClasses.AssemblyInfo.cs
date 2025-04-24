41b6ffba7e9cadfa32773c14bb3b56be90caf17c
Arie Gerard <agerard3@my.gcu.edu>
2025-04-23 19:18:30 -0700

    help

--- MineSweeperClasses/obj/Debug/net8.0-windows/MineSweeperClasses.AssemblyInfo.cs
+++ MineSweeperClasses/obj/Debug/net8.0-windows/MineSweeperClasses.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MineSweeperClasses")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27e035533d6150c0e12f1da78e4a3ac674c5602d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8746451f902cd04b325f9d4ddc18dc709032239")]
 [assembly: System.Reflection.AssemblyProductAttribute("MineSweeperClasses")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MineSweeperClasses")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
